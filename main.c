a0febd2aa0252aa14f3a322df9f5043351051547
PXL <1092187706@qq.com>
2022-07-28 17:28:55 +0800

    算法优化

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
+#include "./files/main.h"
 
 int main()
 {
-    // extern void enter_string(char str[]);
-    // extern void delete_string(char str[], char ch);
-    // extern void print_string(char str[]); //对输入函数，删除函数，输出函数的声明
 
     char c, str[80];
     enter_string(str);
-    scanf("%d", &c); //输入要求删除的字符
+    scanf("%c", &c); //输入要求删除的字符
     delete_string(str, c);
 
     print_string(str);
